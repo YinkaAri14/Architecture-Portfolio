f7dbf18dd13094ee06f97cef34adab11362ad314
YinkaAri14 <aariwa2@lsu.edu>
2024-11-19 10:22:01 -0600

    add slick carousel and photos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 // JS scripts placed here
+$(document).ready(function(){
+    $('.rending-carousel').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+         cssEase: 'linear'
+    });
+  });
+      
+
 // JavaScript to handle the hamburger menu toggle
 document.getElementById('hamburger-icon').addEventListener('click', function() {
     // Toggle menu visibility here, for example:
