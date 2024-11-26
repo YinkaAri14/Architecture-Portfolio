45a6e1efba699f14af8a1a9d216fa3e6f48c73bc
YinkaAri14 <aariwa2@lsu.edu>
2024-11-26 09:56:17 -0600

    Creating external links

--- js/main.js
+++ js/main.js
@@ -18,3 +18,5 @@ hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
 });
+
+document.querySelector('.scroll-container').style.scrollBehavior = 'smooth';
